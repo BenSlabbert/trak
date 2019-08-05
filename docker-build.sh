a75896fa51740a0ca2478c6410b11a7878579554
BenSlabbert <benjamin.slabbert@gmail.com>
2019-08-05 12:04:37 +0200

    adding docker builds for prometheus and logstash

---
+++ docker-build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+VERSION=1.0.1-$(date +%s)
+
+echo "Building Images with version: ${VERSION}"
+
+docker build -t benjaminslabbert/trak_logstash:"${VERSION}" -f ./logstash-conf/Dockerfile ./logstash-conf/
+docker build -t benjaminslabbert/trak_prometheus:"${VERSION}" -f ./prometheus-conf/Dockerfile ./prometheus-conf/
+
+echo "#########################"
+echo "# Pushing to Docker hub #"
+echo "#########################"
+
+echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
+
+docker push benjaminslabbert/trak_logstash:"${VERSION}"
+docker push benjaminslabbert/trak_prometheus:"${VERSION}"
