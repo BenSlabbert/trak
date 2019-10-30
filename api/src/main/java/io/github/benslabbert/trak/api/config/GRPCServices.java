bf7d1bdb2f21c729dbb7639ee86a06be8d80b46f
BenSlabbert <benjamin.slabbert@gmail.com>
2019-10-30 08:38:48 +0200

    configuring api host when it calls itself

--- api/src/main/java/io/github/benslabbert/trak/api/config/GRPCServices.java
+++ api/src/main/java/io/github/benslabbert/trak/api/config/GRPCServices.java
@@ -2,16 +2,20 @@ package io.github.benslabbert.trak.api.config;
 
 import io.github.benslabbert.trak.grpc.ProductServiceGrpc;
 import io.grpc.ManagedChannelBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GRPCServices {
 
+  @Value("${api.host}")
+  private String apiHost;
+
   @Bean
   public ProductServiceGrpc.ProductServiceBlockingStub productServiceBlockingStub() {
     return ProductServiceGrpc.newBlockingStub(
-            ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext().build())
+            ManagedChannelBuilder.forAddress(apiHost, 50051).usePlaintext().build())
         .withCompression("gzip");
   }
 }
